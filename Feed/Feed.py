82cd35c04ed03b5772f12aaa5f28b4297547add2
文皓平 <78809297+algo21-220040079@users.noreply.github.com>
2021-05-07 11:58:36 +0800

    Update Feed.py

--- Feed/Feed.py
+++ Feed/Feed.py
@@ -1,11 +1,12 @@
 import Strategy.Strategy as Strategy
 
 class Feed:
-    def __init__(self,data,coin_number,principal):
+    def __init__(self,data,coin_number,principal，backtest=True):
         self.data = data
         self.coin_number = coin_number #仓位拥有币数
         self.principal = principal #本金
+        self.backtest = backtest
 
     def send_data(self):
-        self.Strategy = Strategy.Strategy(self.data,self.coin_number,self.principal)
+        self.Strategy = Strategy.Strategy(self.data,self.coin_number,self.principal，self.backtest)
         return self.Strategy.analysis()
